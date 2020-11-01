5bfe725191baefb39a9e3e8d9afa8174367b3807
akira uemoto <empty.aauu0001@gmail.com>
2020-11-01 17:54:50 +0900

    bubble sortのコメント追加

--- src/Algorithm/BubbleSort.py
+++ src/Algorithm/BubbleSort.py
@@ -7,6 +7,17 @@ from src.Algorithm.StopWatch import stop_watch
 
 @stop_watch
 class Solution:
+    """
+    bubble sortのイメージ
+    対象データ: 2,1,5,8,7,3
+
+    - 1回目: 2,1,5,7,3 | 8
+    - 2回目: 2,1,5,3 | 7,8
+    - 3回目: 2,1,3 | 5,7,8
+    - 4回目: 2,1 | 3,5,7,8
+    - 5回目: 1 | 2,3,5,7,8
+    """
+
     def bubble_sort(self, nums: List[int]) -> List[int]:
 
         for index, _ in enumerate(nums):
