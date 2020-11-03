fb88b8af2dfb40062b32f51c3644f4e2995002f3
Akira <empty.aauu0001@gmail.com>
2020-11-03 22:03:29 +0900

    #12 Selection Sortの説明追加 (#13)

--- src/Algorithm/SelectionSort.py
+++ src/Algorithm/SelectionSort.py
@@ -4,15 +4,30 @@ from typing import List
 from src.Algorithm.StopWatch import stop_watch
 
 
-@stop_watch
 class Solution:
+
+    @stop_watch
     def select_sort(self, nums: List[int]) -> List[int]:
 
+        '''
+        Select_Sortの説明
+        手前から対象の値を全ての値と比較して、小さい値を昇順に設定する。
+        ＊BubbleSortの改良
+
+        対象データ: 2,1,5,8,7,3
+
+        - 1回目: 2,1,5,8,7,3 -> 1,2,5,8,7,3
+        - 2回目: 1,2,5,8,7,3 -> 1,2,5,8,7,3
+        - 3回目: 1,2,5,8,7,3 -> 1,2,3,8,7,5
+        - 4回目: 1,2,3,8,7,5 -> 1,2,3,5,7,8
+        - 5回目: 1,2,3,5,7,8 -> 1,2,3,5,7,8
+        '''
+
         for index, _ in enumerate(nums):
-            min: int = index
+            minIndex: int = index
             for ckTargetIndex in range(index + 1, len(nums)):
-                if nums[min] > nums[ckTargetIndex]:
-                    min = ckTargetIndex
-            nums[index], nums[min] = nums[min], nums[index]
+                if nums[minIndex] > nums[ckTargetIndex]:
+                    minIndex = ckTargetIndex
+            nums[index], nums[minIndex] = nums[minIndex], nums[index]
 
         return nums
