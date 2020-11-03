f49d899f8aff59b0b08be6bbdf6114dde808bad3
akira uemoto <empty.aauu0001@gmail.com>
2020-11-03 22:37:44 +0900

    #14 Gnome Sortの説明追加

--- src/Algorithm/GnomeSort.py
+++ src/Algorithm/GnomeSort.py
@@ -9,6 +9,19 @@ class Solution:
     @stop_watch
     def gnome_sort(self, nums: List[int]) -> List[int]:
 
+        '''
+        gnome_sortの説明
+
+
+        GnomeSortの改良
+
+        対象データ: 2,1,5,8,7,3
+
+        - 1回目: 2,1,5,8,7,3 -> 1,2,5,8,7,3
+        - 2回目: 1,2,5,8,7,3 -> 1,2,5,8,3,7 -> 1,2,5,3,8,7 -> 1,2,3,5,8,7
+        - 3回目: 1,2,3,5,8,7 -> 1,2,3,5,7,8
+        '''
+
         len_nums: int = len(nums)
         index: int = 0
 
