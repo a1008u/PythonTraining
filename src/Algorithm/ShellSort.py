1e5a508632b8b0c73f831eb89e2e983f45ee8289
Akira <empty.aauu0001@gmail.com>
2020-11-08 16:52:21 +0900

    #20 Shell Sortの作成 (#21)

---
+++ src/Algorithm/ShellSort.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+from typing import List
+
+from src.Algorithm.StopWatch import stop_watch
+
+
+class Solution:
+
+    @stop_watch
+    def shell_sort(self, nums: List[int]) -> List[int]:
+
+        '''
+        Shell_Sortの説明
+        事前にGAPを決めて、そちらを使って一定の区間でsortをしていく
+        ＊Insertion sortの改良
+
+        対象データ: 2,1,5,8,7,3
+
+        - 1回目: 2,1,5,8,7,3 -> 2,1,5,8,7,3 gap=3, temp=8
+        - 2回目: 2,1,5,8,7,3 -> 2,1,5,8,7,3 gap=3, temp=7
+        - 3回目: 2,1,5,8,7,3 -> 2,1,3,8,7,5 gap=3, temp=3
+        - 4回目: 2,1,5,8,7,3 -> 1,2,3,8,7,5 gap=1, temp=1
+        - 5回目: 2,1,5,8,7,3 -> 1,2,3,8,7,5 gap=1, temp=3
+        - 6回目: 2,1,5,8,7,3 -> 1,2,3,7,8,5 gap=1, temp=8
+        - 7回目: 2,1,5,8,7,3 -> 1,2,3,7,5,8 -> 1,2,3,5,7,8 gap=1, temp=5
+        '''
+
+        len_nums: int = len(nums)
+        gap: int = len_nums // 2
+        while gap > 0:
+            for i in range(gap, len_nums):
+                temp = nums[i]
+                j: int = i
+                while j >= gap and nums[j -gap] > temp:
+                    nums[j] = nums[j-gap]
+                    j -= gap
+                nums[j] = temp
+            gap //= 2
+        return nums
---
+++ src/Algorithm/test/test_ShellSort.py
@@ -0,0 +1,24 @@
+import unittest
+import sys, os
+from typing import List
+
+print('prepare test start-----------------------')
+path = os.path.join(os.path.dirname(__file__), '../../../')
+sys.path.append(path)
+sys.path.append('/root/src/')
+print(sys.path)
+print(dir())
+print('prepare test end-----------------------')
+
+from src.Algorithm.ShellSort import Solution
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        nums: List[int] = [6, 15, 4, 2, 8, 5, 11, 9, 7, 13]
+        solution: Solution = Solution()
+        results: List[int] = solution.shell_sort(nums)
+
+        self.assertEqual(sorted(nums), results)
+
+if __name__ == '__main__':
+    unittest.main()
