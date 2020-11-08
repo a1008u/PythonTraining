77f1e7edf92d46961224f171052dee86e92fba05
akira uemoto <empty.aauu0001@gmail.com>
2020-11-08 16:18:54 +0900

    #18 BucketSortのコメント追加

--- src/Algorithm/BucketSort.py
+++ src/Algorithm/BucketSort.py
@@ -5,42 +5,33 @@ from typing import List
 from src.Algorithm import InsertionSort
 from src.Algorithm.StopWatch import stop_watch
 
-
-# def insertion_sort(nums: List[int]):
-#     for index in range(1, len(nums)):
-#         # 現在要素を一時的に記憶する。
-#         temp: int = nums[index]
-#
-#         # 直前の位置を記録する。
-#         targetIndex = index - 1
-#
-#         while (targetIndex >= 0) and (nums[targetIndex] > temp):
-#             nums[targetIndex + 1] = nums[targetIndex]
-#             targetIndex -= 1
-#         nums[targetIndex + 1] = temp
-#     return nums
-
-
 class Solution:
     """
     bubble sortのイメージ
     対象データ: 2,1,5,8,7,3
+    - 最大値：8
 
-    - 1回目: 2,1,5,7,3 | 8
-    - 2回目: 2,1,5,3 | 7,8
-    - 3回目: 2,1,3 | 5,7,8
-    - 4回目: 2,1 | 3,5,7,8
-    - 5回目: 1 | 2,3,5,7,8
+    ### まずはbuckets(2次元配列)に分けましょう
+    - index:0の入れ物[2,1,5,3]
+    - index:1の入れ物[8,7]
+    - index:2の入れ物[]
+    - index:3の入れ物[]
+    - index:4の入れ物[]
+    - index:5の入れ物[]
+
+    ### こちらでInsertion Sortを実行
+    - index:0の入れ物[2,1,5,3] → 1 | 2, 5, 3  → 1 ,2 |  5, 3 ->  1 ,2, 3 | 5 -> 1,2,3,5
+    - index:1の入れ物[8,7] -> 7 | 8 -> 7,8
     """
 
     @stop_watch
     def bucket_sort(self, nums: List[int]) -> List[int]:
 
         max_num: int = max(nums)
-        len_nums: int = len(nums)
-        size: int = max_num // len_nums
+        size: int = 6
 
-        buckets: List[List[int]] = [[] for _ in range(max_num + 1)]
+        # bucketsへsort対象の値を分ける
+        buckets: List[List[int]] = [[] for _ in range(size)]
         for num in nums:
             i = num // size
             if i != size:
@@ -48,12 +39,14 @@ class Solution:
             else:
                 buckets[size - 1].append(num)
 
+        print(buckets)
+
         solution: InsertionSort.Solution = InsertionSort.Solution()
-        for i in range(max_num + 1):
+        for i in range(size):
             solution.insertion_sort(buckets[i])
 
         result: List[int] = []
-        for i in range(max_num + 1):
+        for i in range(size):
             result += buckets[i]
 
         return result
