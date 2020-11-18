9844d4af47b7150788d6b6b8b97ae31aead0dc63
akira uemoto <empty.aauu0001@gmail.com>
2020-11-18 09:53:28 +0900

    #31 BinarySearchを追加

---
+++ src/Algorithm/BinarySearch.py
@@ -0,0 +1,45 @@
+from typing import List, NewType
+
+from src.Algorithm.StopWatch import stop_watch
+
+IndexNum = NewType('IndexNum', int)
+
+
+class Solution:
+
+    @stop_watch
+    def linear_seach(self, nums:List[int], value: int) -> IndexNum:
+        for i in range(0, len(nums)):
+            if nums[i] == value:
+                return i
+        return -1
+
+    @stop_watch
+    def binary_search(self, nums:List[int], value:int) -> IndexNum:
+        left, right = 0, len(nums) -1
+
+        while left <= right:
+            mid:int = (left+right)//2
+            if nums[mid] == value:
+                return mid
+            elif nums[mid] < value:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+
+    @stop_watch
+    def binary_search2(self, nums:List[int], value:int) -> IndexNum:
+        def _binary_search(nums:List[int], value:int, left:IndexNum, right:IndexNum) -> IndexNum:
+            if left > right:
+                return -1
+
+            mid:int = (left+right)//2
+            if nums[mid] == value:
+                return mid
+            elif nums[mid] < value:
+                return _binary_search(nums, value, mid + 1, right)
+            else:
+                return _binary_search(nums, value, left, mid - 1)
+
+        return _binary_search(nums, value, 0, len(nums) - 1)
---
+++ src/Algorithm/test/test_BinarySearch.py
@@ -0,0 +1,66 @@
+import unittest
+import sys, os
+from typing import List
+
+print('prepare test start-----------------------')
+path = os.path.join(os.path.dirname(__file__), '../../../')
+sys.path.append(path)
+sys.path.append('/root/src/')
+print(sys.path)
+print(dir())
+print('prepare test end-----------------------')
+
+from src.Algorithm.BinarySearch import Solution
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        nums: List[int] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+        targetInt: int = 28
+        solution: Solution = Solution()
+        result: int = solution.linear_seach(nums, targetInt)
+
+        self.assertEqual(nums.index(targetInt), result)
+
+    def test_something_2(self):
+        nums: List[int] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+        targetInt: int = 3333
+        solution: Solution = Solution()
+        result: int = solution.linear_seach(nums, targetInt)
+
+        self.assertEqual(-1, result)
+
+    def test_something_3(self):
+        nums: List[int] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+        targetInt: int = 3333
+        solution: Solution = Solution()
+        result: int = solution.binary_search(nums, targetInt)
+
+        self.assertEqual(-1, result)
+
+    def test_something43(self):
+        nums: List[int] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+        targetInt: int = 28
+        solution: Solution = Solution()
+        result: int = solution.binary_search(nums, targetInt)
+
+        self.assertEqual(nums.index(targetInt), result)
+
+    def test_something_5(self):
+        nums: List[int] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+        targetInt: int = 3333
+        solution: Solution = Solution()
+        result: int = solution.binary_search2(nums, targetInt)
+
+        self.assertEqual(-1, result)
+
+    def test_something_6(self):
+        nums: List[int] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+        targetInt: int = 28
+        solution: Solution = Solution()
+        result: int = solution.binary_search2(nums, targetInt)
+
+        self.assertEqual(nums.index(targetInt), result)
+
+if __name__ == '__main__':
+    unittest.main()
