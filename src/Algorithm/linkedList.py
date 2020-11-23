c86a429baeb3299c97b92cfd004b5870a7e89e59
akira uemoto <empty.aauu0001@gmail.com>
2020-11-23 18:55:44 +0900

    #34 追加部分の対応

--- src/Algorithm/linkedList.py
+++ src/Algorithm/linkedList.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any
+from typing import Any, Optional
 
 
 class Node:
@@ -9,6 +9,12 @@ class Node:
 
 
 class LinkedList:
+
+    """
+    - 下記のような形として登録される。
+    - HEAD -> data|next -> data|next -> data|next -> None
+    """
+
     def __init__(self) -> None:
         self.head = None
 
@@ -51,3 +57,60 @@ class LinkedList:
 
         previous_node.next = current_node.next
         current_node = None
+
+    def reverse_iterative(self) -> None:
+        """
+        [1,2,3] -> [3,2,1]に変換する
+
+        """
+        previous_node: Node = None
+        current_node: Node = self.head
+        while current_node:
+            next_node: Node = current_node.next
+            current_node.next = previous_node
+
+            previous_node = current_node
+            current_node = next_node
+
+        self.head = previous_node
+
+    def reverse_recursive(self):
+        def _reverse_recursive(current_node: Node, preious_node: Node) -> None:
+            if not current_node:
+                return preious_node
+
+            next_node: Node = current_node.next
+            current_node.next = preious_node
+            preious_node = current_node
+            current_node = next_node
+            return _reverse_recursive(current_node, preious_node)
+        self.head = _reverse_recursive(self.head, None)
+
+    def reverse_even(self) -> None:
+        """
+        連続する偶数のみを反転する
+        1,4,6,8,9 -> 1,8,6,4,9
+        1,2,3,4,5 -> 1,2,3,4,5
+        :return:
+        """
+        def _reverse_even(head: Node, previous_node: Node) -> Optional[Node]:
+
+            if head is None:
+                return None
+
+            current_node = head
+            while current_node and current_node.data % 2 == 0:
+                next_node: Node = current_node.next
+                current_node.next = previous_node
+                previous_node = current_node
+                current_node = next_node
+
+            if current_node != head:
+                head.next = current_node
+                _reverse_even(current_node, None)
+                return previous_node
+            else:
+                head.next = _reverse_even(head.next, head)
+                return head
+
+        self.head = _reverse_even(self.head, None)
