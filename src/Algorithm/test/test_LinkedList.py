cb7a337ff5e305cbd277afb930ed1605aa8dcfce
akira uemoto <empty.aauu0001@gmail.com>
2020-11-23 18:46:57 +0900

    #34 単方向リストのメソッド追加

--- src/Algorithm/test/test_LinkedList.py
+++ src/Algorithm/test/test_LinkedList.py
@@ -31,6 +31,40 @@ class MyTestCase(unittest.TestCase):
         results.insert(5)
         self.assertEqual(5, results.head.data)
 
+        results.reverse_iterative()
+        expectsreverse: List[int] = [2, 1, 5]
+
+        self.assertEqual(expectsreverse[0], results.head.data)
+        self.assertEqual(expectsreverse[1], results.head.next.data)
+        self.assertEqual(expectsreverse[2], results.head.next.next.data)
+
+        results.reverse_recursive()
+        expectsreverse2: List[int] = [5, 1, 2]
+
+        self.assertEqual(expectsreverse2[0], results.head.data)
+        self.assertEqual(expectsreverse2[1], results.head.next.data)
+        self.assertEqual(expectsreverse2[2], results.head.next.next.data)
+
+        results2: LinkedList = LinkedList()
+        results2.append(2)
+        results2.append(4)
+        results2.append(6)
+        results2.append(1)
+        results2.append(2)
+        results2.append(4)
+        results2.append(6)
+        expectsreverse3: List[int] = [6,4,2,1,6,4,2]
+
+        results2.reverse_even()
+        self.assertEqual(expectsreverse3[0], results2.head.data)
+        self.assertEqual(expectsreverse3[1], results2.head.next.data)
+        self.assertEqual(expectsreverse3[2], results2.head.next.next.data)
+        self.assertEqual(expectsreverse3[3], results2.head.next.next.next.data)
+        self.assertEqual(expectsreverse3[4], results2.head.next.next.next.next.data)
+        self.assertEqual(expectsreverse3[5], results2.head.next.next.next.next.next.data)
+        self.assertEqual(expectsreverse3[6], results2.head.next.next.next.next.next.next.data)
+
+
 
 if __name__ == '__main__':
     unittest.main()
